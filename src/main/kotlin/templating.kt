0f9c7c626a5f56d1b644f51f878282465eacb6c3
kspar <kaspar.papli@gmail.com>
2019-06-04 23:58:17 +0300

    Render templates by id

--- src/main/kotlin/templating.kt
+++ src/main/kotlin/templating.kt
@@ -1,5 +1,11 @@
+import org.w3c.dom.HTMLScriptElement
 
-fun tmRender(template: String, data: Map<String, Any>) =
+
+fun tmRender(templateId: String, data: Map<String, Any>): String =
+    render(getElemByIdAs<HTMLScriptElement>(templateId).text, data)
+
+
+private fun render(template: String, data: Map<String, Any>): String =
     Mustache.render(template, mapToJsObject(data))
 
 
