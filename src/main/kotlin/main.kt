840d83fa93c279851f182fae61fca39833afebd0
kspar <kaspar.papli@gmail.com>
2019-06-05 00:05:50 +0300

    Templating example

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,19 @@
+import org.w3c.dom.HTMLDivElement
 import org.w3c.dom.asList
+import kotlin.dom.appendText
 
 fun main() {
     debug("Yo")
 
-    println(tmRender(""""{{a"}}"-{{b}}""", mapOf("a\"" to "wut", "b" to 3)))
+
+    val first = tmRender("tm-first", mapOf("id" to "firrr"))
+    val second = tmRender("tm-first", mapOf("id" to "seccc"))
+
+    val container = getElemByIdAs<HTMLDivElement>(EasyConst.CONTAINER_ID)
+    container.innerHTML += first
+    container.innerHTML += second
+
+    getElemByIdAs<HTMLDivElement>("firrr").appendText("wuuuuuuuu")
 
 }
 
