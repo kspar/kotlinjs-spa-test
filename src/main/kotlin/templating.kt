1ea6db98ac5f6f6d985571e854088b6ab8e6b143
kspar <kaspar.papli@gmail.com>
2019-06-05 00:01:34 +0300

    Parse templates before rendering

--- src/main/kotlin/templating.kt
+++ src/main/kotlin/templating.kt
@@ -5,8 +5,10 @@ fun tmRender(templateId: String, data: Map<String, Any>): String =
     render(getElemByIdAs<HTMLScriptElement>(templateId).text, data)
 
 
-private fun render(template: String, data: Map<String, Any>): String =
-    Mustache.render(template, mapToJsObject(data))
+private fun render(template: String, data: Map<String, Any>): String {
+    Mustache.parse(template)
+    return Mustache.render(template, mapToJsObject(data))
+}
 
 
 private fun mapToJsObject(map: Map<String, Any>): dynamic {
