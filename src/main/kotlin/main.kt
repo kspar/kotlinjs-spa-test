1c6456cf957a465b46c3e0afd7ca9bd5ba05772a
Kaspar Papli <paplka@f-secure.com>
2019-06-06 17:09:17 +0300

    Ajax response modelling

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,7 +6,10 @@ fun main() {
     val ajaxSettings = mapOf("url" to "https://reqres.in/api/products/4", "method" to "POST")
 
     val r = getCall(ajaxSettings)
-    r.then({ data, status, xhr -> println(data.asDynamic().data.id) }) // TODO: safer/nicer cast to model?
+    r.then({ data, status, xhr ->
+        println(data.unsafeCast<Resp>().data.pantone)
+        println(xhr.status)
+    })
 
 }
 
@@ -16,3 +19,7 @@ fun clearContainer() {
 }
 
 
+// {"data":{"id":4,"name":"aqua sky","year":2003,"color":"#7BC4C4","pantone_value":"14-4811"}}
+data class Resp(val data: RespData)
+data class RespData(val id: Int, val name: String, val year: Int, val color: String,
+                    @JsName("pantone_value") val pantone: String)
